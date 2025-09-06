b0c0f4944781411c5e89b42c8fd817de96837354
Shivam Verma <verma.shivam.cs@gmail.com>
2025-09-06 05:29:10 +0000

    Beginner Problem updated and some stuff from cpp advance class

--- A_In_Search_of_an_Easy_Problem.cpp
+++ A_In_Search_of_an_Easy_Problem.cpp
@@ -1,7 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    
+    int n;
+    cin >> n;
+    int a[n];
+    bool track = false;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == 1)
+        {
+            track = true;
+            break;
+        }
+    }
+    if (track)
+    {
+        cout << "HARD";
+    }
+    else
+    {
+        cout << "EASY";
+    }
 }
---
+++ CppAdvance/setPrecision.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double d = 23.23234;
+    cout << d << endl; // using Vs Code (its default precision is set to 4 decimal places)
+    cout << fixed << setprecision(2) << d << endl;
+    return 0;
+}
