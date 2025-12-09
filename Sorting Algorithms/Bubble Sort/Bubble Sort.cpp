2e740379189c9ab7b6fecd0bc69838a565a44d6d
Shivam-Vermacs <verma.shivam.cs@gmail.com>
2025-12-09 17:15:45 +0530

    Bubble Sort

--- Sorting Algorithms/Bubble Sort/Bubble Sort.cpp
+++ Sorting Algorithms/Bubble Sort/Bubble Sort.cpp
@@ -1,26 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void BubbleSort(vector<int> &vec, int n)
-{
-    for (int i = n - 1; i >= 0; i--)
-    {
-        int swapno = 0;
-        for (int j = 0; j <= i - 1; j++)
-        {
-            if (vec[j] > vec[j + 1])
-            {
-                swap(vec[j], vec[j + 1]);
-                swapno = 1;
-            }
-        }
-        if (swapno == 0)
-        {
-            break;
-        }
-    }
-}
-
 int main()
 {
     int t;
@@ -34,11 +14,21 @@ int main()
         {
             cin >> vec[i];
         }
-        BubbleSort(vec, n);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (vec[j + 1] < vec[j])
+                {
+                    swap(vec[j], vec[j + 1]);
+                }
+            }
+        }
         for (int i = 0; i < n; i++)
         {
             cout << vec[i] << " ";
         }
         cout << endl;
     }
+    return 0;
 }
